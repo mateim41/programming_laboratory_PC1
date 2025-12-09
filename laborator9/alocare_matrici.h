fd8ac53cc57bab3aac3f44d083bea213f9afb759
Mihailov Matei <matei.mihailov@student.tuiasi.ro>
2025-12-09 23:56:45 +0200

    removed duplicates

--- laborator9/alocare_matrici.h
+++
@@ -1,9 +0,0 @@
-#ifndef ALOCARE_MATRICI_H
-#define ALOCARE_MATRICI_H
-
-#include <stddef.h>
-
-void **alocareMatrice(const size_t nrlinii, const size_t nrcoloane, const size_t elemSize);
-void dealocareMatrice(void ***matrix, const size_t nrlinii);
-
-#endif
--- laborator9/p10.h
+++
@@ -1,9 +0,0 @@
-#ifndef P10_H
-#define P10_H
-
-#include <stddef.h>
-
-int **citireMatrice(const size_t n, const size_t m);
-void afisareMatrice(const int **matrix, const size_t n, const size_t m);
-
-#endif
